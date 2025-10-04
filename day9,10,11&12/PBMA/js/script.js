ab730e51e1322166d1372e875b67db8e2037d40b
Rahul Krishnan A <a.rahulkrishnan14@gmail.com>
2025-10-04 12:30:17 +0530

    implementations

---
+++ day9,10,11&12/PBMA/js/script.js
@@ -0,0 +1,18 @@
+const categoryCtx = document.getElementById("categoryChart");
+      new Chart(categoryCtx, {
+        type: "doughnut",
+        data: {
+          labels: ["Food", "Parking"],
+          datasets: [
+            {
+              backgroundColor: ["red", "cornflowerblue"],
+              data: [250, 50],
+            },
+          ],
+        },
+        options: {
+          plugins: {
+            legend: { display: true, position: "bottom" },
+          },
+        },
+      });
