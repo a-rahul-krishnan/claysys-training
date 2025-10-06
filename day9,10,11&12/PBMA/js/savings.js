37cddb08f2cd336638ad1db7aaa8f4f1ff2527a1
Rahul Krishnan A <a.rahulkrishnan14@gmail.com>
2025-10-06 11:17:06 +0530

    implementations

--- day9,10,11&12/PBMA/js/savings.js
+++ day9,10,11&12/PBMA/js/savings.js
@@ -5,3 +5,11 @@ function newGoalPopupOpen(){
 function newGoalPopupClose(){
     document.getElementById("new-goal-popup").style.display = "none";
 }
+
+function updateGoalPopupOpen(){
+    document.getElementById("update-goal-popup").style.display="block";
+}
+
+function updateGoalPopupClose(){
+    document.getElementById("update-goal-popup").style.display = "none";
+}
