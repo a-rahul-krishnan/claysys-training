39a4cee9565ca4d4241bfee5658411c06b2e78d8
Rahul Krishnan A <a.rahulkrishnan14@gmail.com>
2025-10-09 10:43:21 +0530

    Added C# Ex-3

---
+++ day13-20/exercise3/ControlStructures/Program.cs
@@ -0,0 +1,17 @@
+﻿Random random = new Random();
+int target = random.Next(1, 101);
+Console.WriteLine("Guess a number between 1 and 100:");
+int num= Convert.ToInt32(Console.ReadLine());
+while (num != target)
+{
+    if (num < target)
+    {
+        Console.WriteLine("Too low!:");
+    }
+    else if (num > target)
+    {
+        Console.WriteLine("Too high!:");
+    }
+    num = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("You guessed it!");
